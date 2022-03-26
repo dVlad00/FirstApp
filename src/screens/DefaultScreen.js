71a1f148b828f1a4d32445808133756d19a30510
DESKTOP-S4DCVKR\Vlad <dunca.vlad25@gmail.com>
2022-03-26 15:59:19 +0200

    Revert "I did carrousel view."
    This reverts commit 2760b0398c31c2bfa6073cdd4395a00bf0576e91.

--- src/screens/DefaultScreen.js
+++ src/screens/DefaultScreen.js
@@ -1,52 +1,16 @@
-import React, { useState } from "react"
-import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native"
+import React from "react"
+import { View, Text, TouchableOpacity, Image } from "react-native"
 import styles from "./styles/defaultScreenStyles"
-import Carousel from 'react-native-snap-carousel';
-
-
 
 
 const DefaultScreen = ({ navigation }) => {
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
-
-    const img = [
-        {
-            title: "Hello Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id potenti nisl tellus vestibulum dictum luctus cum habitasse augue. Convallis vitae, dictum justo, iaculis id. Cras a ac augue netus egestas semper varius facilisis id. ",
-        }, {
-            title: "Hello2",
-        }, {
-            title: "Hello3"
-        }, {
-            title: "Hello4",
-        }
-    ]
-    const renderItem = ({ item, index }) => {
-        return <View style={styles.renderView}>
-            <Text></Text>  
-            <Text></Text> 
-            <Text style={styles.carrouselText}>{item.title}</Text>
-            {/* <Text>{data.carrouselItem.text}</Text> */}
-        </View>
-    }
-
-
-
     return <View style={styles.allScreenView}>
         <View style={styles.space}></View>
         <View style={styles.imgView}>
             <Image style={styles.img} source={require("../img/photo.png")} />
         </View>
         <View style={styles.carrouselView}>
-            <Carousel
-                contentContainerStyle={styles.carrousel}
-                style={styles.carrousel}
-                ref={null}
-                data={img}
-                renderItem={renderItem}
-                sliderWidth={windowWidth}
-                itemWidth={0.9 * windowWidth}
-            ></Carousel>
+       
         </View>
         <View style={styles.buttonsView}>
             <View style={styles.viewSignIn}>
--- src/screens/styles/defaultScreenStyles.js
+++ src/screens/styles/defaultScreenStyles.js
@@ -2,39 +2,38 @@ import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
     SignIn: {
-        width: "50%",
-        height: "50%",
+        width:"50%",
+        height:"50%",
         backgroundColor: "black",
-        alignSelf: "center",
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10,
-        justifyContent: "center"
+        alignSelf:"center",
+        borderTopLeftRadius:10,
+        borderBottomLeftRadius:10,
+        justifyContent:"center"
     },
     SignUp: {
-        width: "50%",
-        height: "50%",
+        width:"50%",
+        height:"50%",
         backgroundColor: "blue",
-        alignSelf: "center",
-        borderTopRightRadius: 10,
-        borderBottomRightRadius: 10,
-        justifyContent: "center"
+        alignSelf:"center",
+        borderTopRightRadius:10,
+        borderBottomRightRadius:10,
+        justifyContent:"center"
     },
     Text: {
         color: "white",
         alignSelf: "center",
     },
-    space: {
-        flex: 2,
-        backgroundColor: 'white'
+    space:{
+        flex:2,
+        backgroundColor:'white'
     },
     imgView: {
         backgroundColor: "red",
         flex: 4
     },
     carrouselView: {
+        backgroundColor: "white",
         flex: 3,
-        
-       
     },
     buttonsView: {
         flexDirection: "row",
@@ -52,25 +51,15 @@ const styles = StyleSheet.create({
     viewSignIn: {
         backgroundColor: "white",
         flex: 1,
-        justifyContent: "flex-end",
-        flexDirection: "row"
-
-
+        justifyContent:"flex-end",
+        flexDirection:"row"
+        
+        
     },
     viewRegister: {
         backgroundColor: "white",
         flex: 1,
-        flexDirection: "row"
-    },carrousel:{
-        borderTopWidth:"10"
-    },
-    carrouselText:{
-        alignSelf:"center",
-        textAlign:"center"
-    },
-    renderView:{
-        alignItems:"center",
-        justifyContent:"center"
+        flexDirection:"row"
     }
 })
 
