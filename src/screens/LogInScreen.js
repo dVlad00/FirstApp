b66bdd59a77e9d739d1a0bc4578368afaae07cc0
DESKTOP-S4DCVKR\Vlad <dunca.vlad25@gmail.com>
2022-04-13 11:14:17 +0300

    [FST-4] Created Ui for LogIn and Register

--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,23 +1,59 @@
 import React from "react";
-import{View} from "react-native"
-import {Text} from "react-native-elements"
+import { View, Text, Dimensions, Image,TouchableOpacity } from "react-native"
 import Box from "../components/Box"
 import styles from "./styles/logInScreenStyles";
 
-const LogInScreen=()=>{
-    return <View style={{flex:1}}>
-        
+const LogInScreen = ({ navigation }) => {
+
+    return <View style={{ flex: 1, backgroundColor: "white" }}>
+
         <View style={styles.textView}>
-            <Text h1>Lets Sign you in</Text>
-            <Text h4>Welcome Back,</Text>
-            <Text h4>You have been missed</Text>        
+            <Text style={styles.headerText}>Lets Sign you in</Text>
+            <Text style={styles.h2Text}>Welcome Back,</Text>
+            <Text style={styles.h2Text}>You have been missed</Text>
         </View>
         <View style={styles.boxesView}>
-            <Box></Box>
+            <Box setUp={"Email,phone & username"}></Box>
+            <Box setUp={"Password"}
+                password={true}></Box>
+            <TouchableOpacity
+                style={styles.forgotPassword}>
+                <Text style={styles.forgotPasswordText}>Forgot Your Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.SignInButton}>
+                <Text style={styles.SignInText}>Sign In</Text>
+            </TouchableOpacity>
         </View>
-        <View style={styles.orView}></View>
-        <View style={styles.socialView}></View>
-        <View style={styles.buttonToRegisterView}></View>
+        <View style={styles.orView}>
+            <View style={styles.orBar}></View>
+            <View style={styles.orText}>
+                <Text style={styles.orText2}>or</Text>
+            </View>
+            <View style={styles.orBar}></View>
+        </View>
+        <View style={styles.socialView}>
+            <Image
+                style={styles.google}
+                source={require("../img/Google.png")}>
+            </Image>
+            <Image
+                style={styles.facebook}
+                source={require("../img/Facebook.png")}>
+            </Image>
+            <Image
+                style={styles.apple}
+                source={require("../img/Apple.png")}>
+            </Image>
+        </View>
+        <View style={styles.buttonToRegisterView}>
+            <TouchableOpacity
+                style={styles.lastText}
+                onPress={() => navigation.navigate("SignUp")}>
+                <Text>Don`t have and Account?</Text>
+                <Text style={styles.RegisterNow}> Register Now!</Text>
+            </TouchableOpacity>
+        </View>
     </View>
 }
 
---
+++ src/screens/styles/signUpstyles.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const screenWIdth = Dimensions.get("window").width
+const screenHeight = Dimensions.get("window").height
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginHorizontal: 5,
+        flex: 1.5,
+        backgroundColor: "white",
+        marginLeft:15,
+        marginTop:10
+    },
+    boxesView: {
+        backgroundColor: "white",
+        flex: 3,
+        justifyContent: "space-evenly"
+    },
+    buttonView: {
+        backgroundColor: "white",
+        flex: 0.8,
+        justifyContent: "center",
+    },
+    alternativeView: {
+        backgroundColor: "white",
+        flex: 1.5,
+        alignItems: "center"
+    },
+    // Text //
+    headerText: {
+        fontWeight: "bold",
+        fontSize: screenHeight*0.06,
+        color: "black",
+    },
+    header2Text: {
+        fontSize: screenHeight*0.035,
+        color: "black"
+    },
+    // Boxes //
+    // Button//
+    buttonStyle: {
+        backgroundColor: "black",
+        width: screenWIdth * 0.8,
+        height: screenHeight * 0.07,
+        alignSelf: "center",
+        borderRadius: 5,
+        justifyContent: "center",
+        marginTop:10
+    },
+    buttonTextStyle: {
+        color: "white",
+        alignSelf: "center"
+    },
+    //Alternative//
+    alternativeTextView: {
+        flexDirection: "row",
+        margin:15
+    },
+    alternativeTextText: {
+        fontWeight: "bold",
+        color: "black"
+    }
+
+})
+
+export default styles
